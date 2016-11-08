7b0e4999ebce66e6cf9ae7e62ec87ba3e52c5b9b
olgatarr <olga.tarakanowa98@ya.ru>
2016-11-08 20:39:27 +0300

    added hw4 not hw3 :(

---
+++ hw4/hw4 RIGHT.py
@@ -0,0 +1,11 @@
+number = int(input(""))
+while number > 0:
+    word = input("Введите слово: ")
+    if word == 'программирование':
+        break        
+    else:
+        print(word)
+        number = number - 1
+print('Работа программы завершена.')
+
+        
